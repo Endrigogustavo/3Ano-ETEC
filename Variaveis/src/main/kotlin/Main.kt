fb40f0ddf69cd4a668b3d068582c087a62110015
Endrigogustavo <endrigogustavo2006@gmail.com>
2024-02-16 16:38:13 -0300

    m

---
+++ Variaveis/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    //tipos de variavel
+
+    fun variaveis(args: Array<String>){
+        val n1: Byte = 127
+        val n2: Short = 36284.toShort()
+        val n3: Int = 2_147_483_647
+        val n4: Long = 9_
+
+    }
+}
