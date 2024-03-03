886dc468ddfc4af26f191205babe969dfc274d89
/__Endrigo__ <113954684+Endrigogustavo@users.noreply.github.com>
2024-03-02 21:49:20 -0300

    Update BdColegio.sql

--- BD II/05-03/BdColegio.sql
+++ BD II/05-03/BdColegio.sql
@@ -62,7 +62,7 @@ values ('Tecnico de Infromatica',31,32,33);
 
 create table Alunos( 
 
-Matricula int identity, 
+Matricula int, 
 Nome varchar(55)  Not Null, 
 Endereco varchar(105),
 Cidade varchar(55), 
@@ -74,8 +74,23 @@ foreign key (CodCurso) references Cursos
 ) 
 
 
-insert into TblCursos(NomeCurso, CodProfessor)
-values ('RH',4);
+insert into Alunos(Matricula, Nome, Endereco, Cidade, CodCurso)
+values 
+ (1001,'Marcos Moraes','Rua Pe Roque, 2057','Mogi Mirim',01),
+ (1002,'Maria Conceição Lopes','Rua Araras, 23','Mogi Guaçu',01),
+ (1003,'Ana Carina Lopes','Rua Peraltas, 222','Santos',01),
+ (1004,'Carlos Aguiar','Rua Botafogo, 33','Santos',01),
+ (1005,'André Luiz dos Santos','Rua Lopes Conte, 3343','Sapucaí',01),
+ (1006,'Pedro Antonio Pimenta','Rua Altair Lopes, 33','Itapira',02),
+ (1007,'Rita de Cássia da Silva','Rua Eletromais, 33','Itapira',02),
+ (1008,'Caique dos Santos','Rua das Amoreiras, 55','Campinas',02),
+ (1009,'Carlos Tavares','Rua Peixe, 99','Santos',02),
+ (1010,'Antonio Carlos Caetano','Rua Josefina Alface, 987','Campinas',02),
+ (1011,'Ricardo Moreira','Rua do Pinhal, 332','Aparecida',03),
+ (1012,'Richardson S. P. Campeao','Rua do Tricolor, 433','São Paulo',03),
+ (1013,'Junior Camisa Seis','Rua do Morumbi, 433','São Paulo',03),
+ (1014,'Carina Melo','Rua Osvaldo Ramos, 88','Mogi Guaçu',03),
+ (1015,'Pedro Mello','Rua Itororó, 3999','Mogi Mirim',03);
 
   
 
