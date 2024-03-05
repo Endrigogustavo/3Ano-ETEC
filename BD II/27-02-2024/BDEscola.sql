7c97456188cd5f6aa1ca67601cb8f8b9cd6ec723
/__Endrigo__ <113954684+Endrigogustavo@users.noreply.github.com>
2024-03-05 15:17:21 -0300

    Update BDEscola.sql

--- BD II/27-02-2024/BDEscola.sql
+++ BD II/27-02-2024/BDEscola.sql
@@ -1,95 +1,186 @@
- create database BdColegio
- use BdColegio
+ create database BdEscola
+ use BdEscola
 
 
-create table Disciplinas( 
+create table TblAluno( 
 
-CodDisciplinas int,
-NomeDisciplinas varchar(55) Not Null,
+CodAluno int identity,  
+Nome varchar(55) Unique Not Null,
+Endereco varchar(155) Unique Not Null,
+Telefone varchar(13) Unique Not Null,
+Nascimento varchar(10)  Not Null,
+Pai varchar(55)  Not Null,
+Mae varchar(55)  Not Null,
+Responsavel varchar(55)  Not Null,
 
-primary key (CodDisciplinas)
+primary key (CodAluno) 
 
 ) 
 
-insert into Disciplinas(CodDisciplinas, NomeDisciplinas)
-values (11,'BD I');
-insert into Disciplinas(CodDisciplinas, NomeDisciplinas)
-values (12,'TPA');
-insert into Disciplinas(CodDisciplinas, NomeDisciplinas)
-values (13,'DS I');
-insert into Disciplinas(CodDisciplinas, NomeDisciplinas)
-values (21,'BD II');
-insert into Disciplinas(CodDisciplinas, NomeDisciplinas)
-values (22,'DS II');
-insert into Disciplinas(CodDisciplinas, NomeDisciplinas)
-values (23,'PC I');
-insert into Disciplinas(CodDisciplinas, NomeDisciplinas)
-values (31,'BD III');
-insert into Disciplinas(CodDisciplinas, NomeDisciplinas)
-values (32,'PC II');
-insert into Disciplinas(CodDisciplinas, NomeDisciplinas)
-values (33,'DS III');
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Danilo','Aricanduva', '1199999999', '2005' , 'valdir', 'meire', 'Danilo');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Nickolas','Rua Pereira Estéfano', '1199999991', '2007' , 'Julio Cesar', 'mae top', 'Giovani');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Alex','Avenida Governador José Malcher', '1199999992', '2005' , 'Arthur', 'Evelyn', 'Evelyn');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Endrigo','Praça da República 22', '1199999988', '2005' , 'Benjamin', 'Violet', 'Violet');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Pedro','Travessa da CDL', '1199999993', '2005' , 'Samuel', 'Eleanor', 'Eleanor');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Valentim','Avenida Rio Branco', '1199999994', '2004' , 'Elias', 'Hazel', 'Elias');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Mateo','Avenida Esbertalina Barbosa Damiani', '1199999995', '2005' , 'valdir', 'Charlotte', 'valdir');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Thor','QE 11 Área Especial C', '1199999996', '2005' , 'Walter', 'Alice', 'Alice');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Leo','Avenida Afonso Pena', '1199999997', '2005' , 'Leonard', 'Sophia', 'Sophia');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Zyan','Rua Serra de Bragança', '1199999998', '2007' , 'Ronaldo', 'Penelope', 'Ronaldo');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Bruno','Rua Cristiano Olsen', '1199999980', '2005' , 'Cleber', 'Stella', 'Stella');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Igor','Rua Arlindo Nogueira', '1199999981', '2007' , 'vanderley', 'Clara', 'Clara');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Enrico','Rua Tenente-Coronel Cardoso', '1199999982', '2006' , 'Pablo', 'Julia', 'Julia');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Otto','Rua da Imprensa', '1199999983', '2005' , 'Carlos', 'Ana', 'Carlos');
+
+insert into TblAluno(Nome, Endereco, Telefone, Nascimento, Pai, Mae, Responsavel)
+values ('Lucas','Praça da República', '1199999984', '2006' , 'Wellinton', 'Clara', 'Wellinton');
 
 
+create table TblProfessor( 
 
-
-
-
-
-
-create table Cursos( 
-
-CodCurso int identity ,
+CodProfessor int identity, 
 Nome varchar(55)  Not Null, 
-CodDisc1 int,
-CodDisc2 int,
-CodDisc3 int,
+Endereco varchar(155)  Not Null,
+
+primary key (CodProfessor) 
+
+) 
+
+insert into TblProfessor(Nome, Endereco)
+values ('Marlon','Itaquera');
+
+insert into TblProfessor(Nome, Endereco)
+values ('Edna','Tatuapé');
+
+insert into TblProfessor(Nome, Endereco)
+values ('Carlos','Aricanduva');
+
+insert into TblProfessor(Nome, Endereco)
+values ('Rafael','Artur Alvim');
+
+insert into TblProfessor(Nome, Endereco)
+values ('Collado','Itaquera');
+
+insert into TblProfessor(Nome, Endereco)
+values ('Esmeralda','Consolação');
+
+insert into TblProfessor(Nome, Endereco)
+values ('Rogerio','Paraiso');
+
+create table TblCursos( 
+
+CodCurso int identity, 
+NomeCurso varchar(25)  Not Null,  
+CodProfessor int, 
 
 primary key (CodCurso) ,
-foreign key (CodDisc1) references Disciplinas,
-foreign key (CodDisc2) references Disciplinas,
-foreign key (CodDisc3) references Disciplinas
+foreign key (CodProfessor) references TblProfessor 
 
 ) 
 
 
-insert into Cursos(Nome, CodDisc1 ,CodDisc2,CodDisc3)
-values ('Auxiliar de Informatica',11,12,13);
-insert into Cursos(Nome, CodDisc1 ,CodDisc2,CodDisc3)
-values ('Programador',21,22,23);
-insert into Cursos(Nome, CodDisc1 ,CodDisc2,CodDisc3)
-values ('Tecnico de Infromatica',31,32,33);
+insert into TblCursos(NomeCurso, CodProfessor)
+values ('DS',1);
 
-create table Alunos( 
+insert into TblCursos(NomeCurso, CodProfessor)
+values ('ADM',2);
 
-Matricula int, 
-Nome varchar(55)  Not Null, 
-Endereco varchar(105),
-Cidade varchar(55), 
-CodCurso int, 
+insert into TblCursos(NomeCurso, CodProfessor)
+values ('LOG',3);
 
-primary key (Matricula) ,
-foreign key (CodCurso) references Cursos
-
-) 
-
-
-insert into Alunos(Matricula, Nome, Endereco, Cidade, CodCurso)
-values 
- (1001,'Marcos Moraes','Rua Pe Roque, 2057','Mogi Mirim',01),
- (1002,'Maria Conceição Lopes','Rua Araras, 23','Mogi Guaçu',01),
- (1003,'Ana Carina Lopes','Rua Peraltas, 222','Santos',01),
- (1004,'Carlos Aguiar','Rua Botafogo, 33','Santos',01),
- (1005,'André Luiz dos Santos','Rua Lopes Conte, 3343','Sapucaí',01),
- (1006,'Pedro Antonio Pimenta','Rua Altair Lopes, 33','Itapira',02),
- (1007,'Rita de Cássia da Silva','Rua Eletromais, 33','Itapira',02),
- (1008,'Caique dos Santos','Rua das Amoreiras, 55','Campinas',02),
- (1009,'Carlos Tavares','Rua Peixe, 99','Santos',02),
- (1010,'Antonio Carlos Caetano','Rua Josefina Alface, 987','Campinas',02),
- (1011,'Ricardo Moreira','Rua do Pinhal, 332','Aparecida',03),
- (1012,'Richardson S. P. Campeao','Rua do Tricolor, 433','São Paulo',03),
- (1013,'Junior Camisa Seis','Rua do Morumbi, 433','São Paulo',03),
- (1014,'Carina Melo','Rua Osvaldo Ramos, 88','Mogi Guaçu',03),
- (1015,'Pedro Mello','Rua Itororó, 3999','Mogi Mirim',03);
+insert into TblCursos(NomeCurso, CodProfessor)
+values ('RH',4);
 
   
+
+
+
+create table TblNotas( 
+
+CodAluno int , 
+CodCurso int , 
+Nota int Not Null, 
+Ano int Not Null, 
+Bimestre int, 
+Observacao varchar(100),
+  
+foreign key (CodAluno) references TblAluno,
+foreign key (CodCurso) references TblCursos 
+) 
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (1,1,10,2000,1,'nada');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (2,2,7,2002,1,'Melhore');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (3,3,9,2010,1,'nada');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (4,4,2,2000,1,'Foi de F');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (5,1,5,2000,1,'Vala');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (6,2,10,2000,1,'Fez o minimo');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (7,3,10,2000,1,'Fez o minimo');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (8,4,8,2000,1,'Melhore');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (9,1,7,2000,1,'Melhore');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (10,2,1,2000,1,'Te vejo no céu irmao');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (11,3,5,2000,1,'Melhore');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (12,4,6,2000,1,'Melhore');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (13,1,10,2000,1,'KKKKK');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (14,2,10,2000,1,'Colando é facil');
+
+insert into TblNotas(CodAluno, CodCurso, Nota, Ano, Bimestre, Observacao)
+values (15,3,10,2000,1,'GG izi');
+
+SELECT *from TblAluno
+SELECT *from TblCursos
+SELECT *from TblNotas
+SELECT *from TblProfessor
