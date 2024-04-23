9cdf23d8f82901e345721d2e9c78015c408f2c7f
/__Endrigo__ <113954684+Endrigogustavo@users.noreply.github.com>
2024-04-23 15:48:51 -0300

    Update Funcionarios.sql

--- BD II/Atv5/Funcionarios.sql
+++ BD II/Atv5/Funcionarios.sql
@@ -27,35 +27,6 @@ Create table Funcionarios(
 	  FOREIGN KEY (CodDepartamento) REFERENCES Departamentos
 );
 
-CREATE DATABASE BdEmpresa
-USE BdEmpresa
-
-Create table Departamentos(
-	Codigo_depto int,
-	NomeDepto varchar(20),
-	PRIMARY KEY (Codigo_depto)
-);
-
-
-Create table Funcionarios(
-	Codigo_func Int,
-	NomeFunc Varchar(55),
-	SobreNome Varchar(55),
-	DataNasci Date,
-	CPF varchar(20),
-	RG varchar(30),
-	Endereco varchar(70),
-	CEP varchar(10),
-	Cidade varchar(20),
-	Fone varchar(14),
-	Funcao varchar(30),
-	Salario Float,
-	CodDepartamento int,
-
-	  PRIMARY KEY (Codigo_func),
-	  FOREIGN KEY (CodDepartamento) REFERENCES Departamentos
-);
-
 
 INSERT INTO Departamentos (Codigo_depto, NomeDepto) VALUES
 (1, 'Logistica'),
@@ -73,3 +44,42 @@ INSERT INTO Funcionarios (Codigo_func, NomeFunc, SobreNome, DataNasci, CPF, RG, 
 (8, 'Fernanda', 'Almeida', '1982-04-12', '222.333.444-55', '222333444', 'Rua H, 456', '98765-432', 'Cidade B', '222333444', 'Desenvolvedor', 6000.00, 2),
 (9, 'Rafael', 'Gomes', '1991-06-28', '666.555.444-33', '666555444', 'Rua I, 789', '24680-135', 'Cidade C', '666555444', 'Gerente', 8000.00, 3),
 (10, 'Patrícia', 'Pereira', '1987-02-14', '333.444.555-66', '333444555', 'Rua J, 123', '13579-246', 'Cidade A', '333444555', 'Analista', 5000.00, 1);
+
+--Select's Base
+Select * from Departamentos;
+Select * from Funcionarios;
+
+--Exercicio 1
+Select NomeFunc, Sobrenome from Funcionarios order by Sobrenome;
+
+--Exercicio 2
+Select * from Funcionarios order by Cidade;
+
+--Exercicio 3
+Select * from Funcionarios where Salario>1000 order by NomeFunc;
+
+--Exercicio 4
+Select Datanasci, NomeFunc from Funcionarios order by Datanasci desc;
+
+--Exercicio 5
+Select Sobrenome, NomeFunc, Cidade, Endereco, Fone from Funcionarios;
+
+--Exercicio 6
+Select sum(salario) from Funcionarios;
+
+--Exercicio 7
+Select NomeFunc, Salario, Funcao from Funcionarios order by Funcao;
+
+--Exercicio 8
+-- Supervisos = Desenvolvedor
+Select * from Funcionarios where Funcao like 'Desenvolvedor'
+
+--Exercicio 9
+select count(Codigo_func) from Funcionarios;
+
+--Exercicio 10
+Select avg(Salario) from Funcionarios;
+
+--Exercicio 11
+Select NomeFunc from Funcionarios where Cidade like 'Cidade A' and Funcao like 'Analista';
+
